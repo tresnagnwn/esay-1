e53853fbeeca66040e829bfe01bcee34009f935a
tresnagnwn <151417461+tresnagnwn@users.noreply.github.com>
2023-11-19 10:11:25 +0700

    Add files via upload
    tresn

---
+++ perpangkatan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author fazri
+ */
+public class perpangkatan {
+    public static void main(String[] args) {
+        int bil =3;
+        int pangkat =3;
+        int hasil =1;
+        for (int i = 1; i <=pangkat; i++) {
+            hasil = hasil * bil;
+            
+        }
+        System.out.println(bil + "pangkat" + pangkat +"," + hasil);
+    }
+    
+}
